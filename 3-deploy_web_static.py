b6991056d54895656afc5477cc0a3e775f525fa4
Smolclin <clinsmol10@gmail.com>
2024-05-17 01:47:37 -0700

    Fix Errors

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -10,26 +10,26 @@ env.hosts = ['54.236.52.43', '54.237.96.155']
 
 
 def do_pack():
-    '''generates a tgz archive'''
+    """generates a tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir('versions') is False:
-            local('mkdir versions')
-        file_name = 'versions/web_static_{}.tgz'.format(date)
-        local('tar -cvzf {} web_static'.format(file_name))
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
         return file_name
     except:
         return None
 
 
 def do_deploy(archive_path):
-    '''distribute an archive to the web servers'''
+    """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
     try:
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
-        path = '/data/web_static/releases/'
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_ext))
         run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
@@ -44,7 +44,7 @@ def do_deploy(archive_path):
 
 
 def deploy():
-    '''create and distribute an archive to the web servers'''
+    """creates and distributes an archive to the web servers"""
     archive_path = do_pack()
     if archive_path is None:
         return False
