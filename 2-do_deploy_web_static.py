38d6934f00c8a3c5e731e8bccf668cae628af182
Smolclin <clinsmol10@gmail.com>
2024-05-16 23:49:26 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,20 +10,20 @@ env.hosts = ['54.236.52.43, 54.237.96.155']
 
 def do_deploy(archive_path):
     ''' distribut an archive to web servers'''
-    if exists(achive_path) is False:
+    if exists(archive_path) is False:
         return False
     try:
         file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
+        no_exit = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('mkdir -p {}{}/'.format(path, no_exit))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_exit))
         run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}[1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('mv {0}{1}/web_static/* {0}[1}/'.format(path, no_exit))
+        run('rm -rf {}{}/web_static'.format(path, no_exit))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_exit))
         return True
     except:
         return False
