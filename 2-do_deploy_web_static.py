6d17d6ae353e5e60ebbbb876c015ae83e60655df
Smolclin <clinsmol10@gmail.com>
2024-05-17 00:02:06 -0700

    Fix Error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,13 +17,20 @@ def do_deploy(archive_path):
         no_exit = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_exit))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_exit))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
         run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}[1}/'.format(path, no_exit))
-        run('rm -rf {}{}/web_static'.format(path, no_exit))
+        run('mv {0}{1}/web_static/* {0}[1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_exit))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
+
+    def deploy():
+        ''' Creates and distributes archive to the web server'''
+        archive_path = do_pack()
+        if archive_path is None:
+            return False
+        return do_deploy(archive_path)
